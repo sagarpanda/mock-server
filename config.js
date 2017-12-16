4d7882e95c97ae3828d4b6ab802bb11d57b7c4e0
Sagar Panda <er.sagarpanda@gmail.com>
2017-12-16 07:53:18 +0000

    update port

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port: 3000,
+  port: process.env.PORT ? process.env.PORT : 3000,
   header: {
     // Website you wish to allow to connect
     "Access-Control-Allow-Origin": "*",
